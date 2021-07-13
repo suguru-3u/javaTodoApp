ff4d4ffa4bfbfceae5709080d58f7bd7a60d985e
Oohira Suguru <suguru.13579515@gmail.com>
2021-07-14 06:26:01 +0900

    change main.java

--- javaTodoApp/src/main/Main.java
+++ javaTodoApp/src/main/Main.java
@@ -44,14 +44,6 @@ public class Main{
           user.memoContentEdit();
           break;
 
-        case 4 :
-          user.memoContentEdit();
-          break;
-
-        case 6 :
-          user.memoContentEdit();
-          break;
-
         case 5 :
           app = false;
           break;
